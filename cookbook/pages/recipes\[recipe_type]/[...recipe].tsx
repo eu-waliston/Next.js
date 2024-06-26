43a2cd746c6f322466a4d4a3ef1168aa63c30a61
Waliston <wesantosdev@gmail.com>
2024-06-26 19:45:21 -0300

    end od chapter 01

--- cookbook/pages/recipes\[recipe_type]/[...recipe].tsx
+++ cookbook/pages/recipes\[recipe_type]/[...recipe].tsx
@@ -1,17 +1,32 @@
 import { useRouter } from "next/router";
+import React from "react";
 
-const Recipe = () => {
+const Recipe = ({ initialData }) => {
     const router = useRouter()
-    const { recipes, /*recipe_type */ } = router.query
+    const { recipes, comment_page /*recipe_type */ } = router.query
 
     let label = "Next Recipe"
     let link = "/recipes/breakfast/HealthyBreakfast"
+    let default_comment_pages_value = Number(comment_page) ?? 10
 
-    if(recipes && recipes[2] === "HealthyBreakfast") {
+    if (recipes && recipes[2] === "HealthyBreakfast") {
         label = "Previous Recipe"
         link = "/recipes/breakfast/AnotherHealthyBreakfast"
     }
 
+    React.useEffect(() => {
+        //Init some default value
+        router.push(link + '?comment_page' + default_comment_pages_value, undefined, { shallow: true })
+    }, [])
+
+    React.useEffect(() => {
+        console.log(comment_page, initialData);
+    }, [comment_page, initialData])
+
+
+    const nextPageNumber = (page: string | string[] | undefined): number => {
+        return Number(page) + 1
+    }
     return <div>
         Recipe: in {
             recipes && recipes.map((recipe: string, index: number) => {
@@ -25,7 +40,15 @@ const Recipe = () => {
         <button onClick={() => router.push(link)}>
             {label}
         </button>
+        <button onClick={() => router.push(link + '?comment_page' + nextPageNumber(comment_page), undefined, { shallow: true })}>
+            Change comment page
+        </button>
     </div>
+}
+
+Recipe.getInitialProps = () => {
+    const initialData =  "data on load: " + Math.random()
+    return {initialData} 
 }
 
 export default Recipe
