fddfac31462e202ce281841b51e0d3e93fa9f689
Waliston <wesantosdev@gmail.com>
2024-07-11 10:58:03 -0300

    update

--- 02-different-rendering-strategies/client-side-rendering/pages/_app.js
+++
@@ -1,5 +0,0 @@
-import "../styles/Main.css";
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
--- 02-different-rendering-strategies/client-side-rendering/pages/index.Page.jsx
+++
@@ -1,7 +0,0 @@
-//Using the process.browser variable
-
-function IndexPage() {
-  const side = process.browser ? "client" : "server";
-  return <div>You're currently on the {side}-side.</div>;
-}
-export default IndexPage;
